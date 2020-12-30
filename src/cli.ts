4d9c72197008ff583cb53525a54e56e711820a9c
Asuka109 <1097198892@qq.com>
2020-12-30 17:30:04 +0800

    feat: 文件变更时重新构建

--- src/cli.ts
+++ src/cli.ts
@@ -12,20 +12,28 @@ const removeDuplicate = <T = any>(arr: T[]) => Array.from(new Set(arr))
 
 const cliAction = (cliOptions: CliArgs, watch: boolean) => {
   const options = mergeCliArgs(readConfig(), cliOptions)
-  const getOutputPagePath = (filename: string) => {
-    const relativePath = path.relative(pages, filename)
-    return path.resolve(`${options.output.dir}/${relativePath}`)
-  }
   const {
     input: { components, layouts, pages },
     devServer: { watch: _watch }
   } = options
-  const newx = new Newx({ ...options.input, format: options.output.format })
+  const getOutputPagePath = (filename: string) => {
+    const relativePath = path.relative(pages, filename)
+    return path.resolve(`${options.output.dir}/${relativePath}`)
+  }
+  const newx = new Newx({
+    ...options.input,
+    format: options.output.format,
+    parser: options.parser,
+    render: options.render
+  })
   if (options.output.clean)
     rimraf.sync(options.output.dir)
 
   // Build all pages.
-  glob.sync(`${pages}/**/*.html`).forEach(inputFile => {
+  const pageFiles = glob.sync(`${pages}/**/*.html`).map(filename => path.resolve(filename))
+
+  console.log('pageFiles: ', pageFiles);
+  pageFiles.forEach(inputFile => {
     const outputFile = getOutputPagePath(inputFile)
     newx.processFile(inputFile, outputFile)
   })
@@ -33,11 +41,20 @@ const cliAction = (cliOptions: CliArgs, watch: boolean) => {
 
   // Watch dependent files and recompile them when they are modified.
   const watchFiles = removeDuplicate([
-    components, layouts, ..._watch instanceof Array ? _watch : [_watch]
+    pages, components, layouts, ..._watch instanceof Array ? _watch : [_watch]
   ])
-  watchFiles.forEach(path => {
-    fs.watch(path, (event, filename) => {
-      newx.processFile(filename, getOutputPagePath(filename))
+  watchFiles.forEach(watchFile => {
+    fs.watch(watchFile, (event, filename) => {
+      const abspath = path.resolve(watchFile, filename)
+      console.log('abspath: ', abspath)
+      if (pageFiles.includes(abspath)) {
+        newx.processFile(abspath, getOutputPagePath(abspath))
+      } else {
+        pageFiles.forEach(inputFile => {
+          const outputFile = getOutputPagePath(inputFile)
+          newx.processFile(inputFile, outputFile)
+        })
+      }
     })
   })
 }
